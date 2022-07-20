8dedab916c32e464fb8eee8ffb461e412522d29b
Kiko <kikoguirado@gmail.com>
2022-07-20 10:14:48 +0200

    installed primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Kiko';
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
